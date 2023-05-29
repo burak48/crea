e7027cfc14634ce27a8baf990c83ec9992b5783a
burak48 <buraksaglam3@gmail.com>
2023-05-30 00:39:43 +0300

    mock data created

---
+++ backend/productDetailDataGenerator.js
@@ -0,0 +1,41 @@
+import { faker } from "@faker-js/faker";
+import fs from "fs";
+
+const generateComments = (count) => {
+  const comments = [];
+
+  for (let i = 0; i < count; i++) {
+    const comment = {
+      id: faker.string.uuid(),
+      username: faker.internet.userName(),
+      comment: faker.lorem.sentence(5),
+      rating: faker.number.int({ min: 0, max: 5 }),
+    };
+
+    comments.push(comment);
+  }
+
+  return comments;
+};
+
+const generateProductDetail = (count) => {
+  const productDetail = [];
+
+  for (let i = 0; i < count; i++) {
+    const detail = {
+      id: faker.string.uuid(),
+      name: faker.lorem.words(),
+      description: faker.lorem.paragraphs(),
+      price: faker.finance.amount({ min: 0, max: 99, dec: 2, symbol: "$" }),
+      arrivalDate: faker.date.recent(),
+      comments: generateComments(faker.number.int({ min: 1, max: count })),
+    };
+
+    productDetail.push(detail);
+  }
+
+  return productDetail;
+};
+
+const mockData = generateProductDetail(7);
+fs.writeFileSync("./productDetailData.json", JSON.stringify(mockData));
---
+++ backend/productListDataGenerator.js
@@ -0,0 +1,27 @@
+import { faker } from "@faker-js/faker";
+import fs from "fs";
+
+const generateProducts = (count) => {
+  const productList = [];
+
+  for (let i = 0; i < count; i++) {
+    const product = {
+      id: faker.string.uuid(),
+      name: faker.lorem.words(),
+      price: faker.finance.amount({ min: 0, max: 99, dec: 2 }),
+      score: faker.number.int({ min: 0, max: 5 }),
+      image: faker.image.urlLoremFlickr({
+        category: "nature",
+        width: 320,
+        height: 180,
+      }),
+    };
+
+    productList.push(product);
+  }
+
+  return productList;
+};
+
+const mockData = generateProducts(7);
+fs.writeFileSync("./productListData.json", JSON.stringify(mockData));
