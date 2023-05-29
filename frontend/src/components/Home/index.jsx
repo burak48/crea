a9cb521fe767836c0d48994b7c2d26adeea46997
burak48 <buraksaglam3@gmail.com>
2023-05-29 23:29:54 +0300

    Header component created

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -1,24 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import Header from "../Header";
 import ProductList from "../ProductList";
 
 function Home() {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    navigate("/login");
-  };
-
   return (
     <div>
-      <header className="bg-gray-200 p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">CREA</h1>
-        <button
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-          onClick={handleLogout}
-        >
-          Logout
-        </button>
-      </header>
+      <Header />
       <ProductList />
     </div>
   );
