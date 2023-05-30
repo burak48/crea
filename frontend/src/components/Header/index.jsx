f60cff8e0616cce92736df483b799e1c36d429a2
burak48 <buraksaglam3@gmail.com>
2023-05-30 22:05:57 +0300

    Header component logic changed

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,22 +1,35 @@
-import {useNavigate} from 'react-router-dom'
+import {useEffect} from 'react'
+import {useNavigate, Link} from 'react-router-dom'
 
 function Header() {
     const navigate = useNavigate()
 
     const handleLogout = () => {
+        localStorage.removeItem('token')
         navigate('/login')
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (!token) {
+            navigate('/login')
+        }
+    }, [navigate])
 
     return (
         <div>
             <header className="bg-gray-100 p-4 flex justify-between items-center">
-                <h1 className="text-2xl font-bold">CREA</h1>
-                <button
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-                    onClick={handleLogout}
-                >
-                    Logout
-                </button>
+                <h1 className="text-2xl font-bold">
+                    <Link to={localStorage.getItem('token') ? '/home' : '/login'}>CREA</Link>
+                </h1>
+                {localStorage.getItem('token') && (
+                    <button
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                        onClick={handleLogout}
+                    >
+                        Logout
+                    </button>
+                )}
             </header>
         </div>
     )
